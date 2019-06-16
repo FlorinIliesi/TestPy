8f58bc4171e2cdac9a91834b40321b8dbf20cd35
FlorinIliesi <50583155+FlorinIliesi@users.noreply.github.com>
2019-06-16 23:04:31 +0200

    edited file_test.py

--- file_test.py
+++ file_test.py
@@ -1,1 +1,12 @@
 print("Hello world")
+
+endpoint = 10
+
+def loop(endpoint):
+    while True:
+        if enpoint != 0:
+            endpoint -= 1
+        else:
+            print(endpoint, ' is the limit')
+
+
