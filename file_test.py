c2bc9b07b2a580ca6dc44be6ae56962abc4e168e
FlorinIliesi <50583155+floriniliesi@users.noreply.github.com>
2019-06-16 23:15:10 +0200

    files were updated and tested
    Signed-off-by: FlorinIliesi <50583155+FlorinIliesi@users.noreply.github.com>

--- file_test.py
+++ file_test.py
@@ -6,5 +6,9 @@ def loop(endpoint):
     while True:
         if endpoint != 0:
             endpoint -= 1
+            print(endpoint)
         else:
             print(endpoint, ' is the limit')
+            break
+
+loop(10)
